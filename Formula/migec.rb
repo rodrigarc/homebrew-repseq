01c7bc71f368461e01b229a454dc1b4e7f222784
mikessh <mikhail.shugay@gmail.com>
2016-04-09 02:42:43 +0300

    Update sha - fixed permissions in migec release

--- Formula/migec.rb
+++ Formula/migec.rb
@@ -3,7 +3,7 @@ class Migec < Formula
   homepage "http://migec.readthedocs.org/en/latest/index.html"
   url "https://github.com/mikessh/migec/releases/download/1.2.4/migec-1.2.4.zip"
   version "1.2.4"
-  sha256 "9d29ec2e50b4751612d30decdcdcb6f96af57a03c65b89051eef3de1b0fc95aa"
+  sha256 "f8ebf854ee5d5f77dc42c97fc0166ccce8b838d77abd3c94cb3a7e7e85afd244"
 
   depends_on :java
 
