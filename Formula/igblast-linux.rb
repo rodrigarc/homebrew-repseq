705b0fd704830bfb588441f1c964425e28cd5ac8
mikessh <mikhail.shugay@gmail.com>
2016-04-09 20:30:08 +0300

    2prev

--- Formula/igblast-linux.rb
+++ Formula/igblast-linux.rb
@@ -1,11 +1,11 @@
-class IgblastMacOsx < Formula
+class IgblastLinux < Formula
   desc "IgBlast: A tool for immunoglobulin (IG) and T cell receptor (TR) V domain sequences."
   homepage "http://www.ncbi.nlm.nih.gov/igblast/igblast.cgi"
-  url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-macosx.zip"
+  url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-linux-x64.zip"
   version "1.4.0"
   sha256 "1ef7dd83d0f90ede2c2982a641fc27b6d58b1e8d448e985913b210747253ed63"
 
-  conflicts_with "igblast-linux-x64", :because => "also ships igblastn, igblastp and makeblastdb binaries"
+  conflicts_with "igblast-macos", :because => "also ships igblastn, igblastp and makeblastdb binaries"
 
   def install
     prefix.install Dir["*"]
--- Formula/igblast-macos.rb
+++ Formula/igblast-macos.rb
@@ -1,11 +1,11 @@
-class IgblastLinuxX64 < Formula
+class IgblastMacos < Formula
   desc "IgBlast: A tool for immunoglobulin (IG) and T cell receptor (TR) V domain sequences."
   homepage "http://www.ncbi.nlm.nih.gov/igblast/igblast.cgi"
-  url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-linux-x64.zip"
+  url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-macosx.zip"
   version "1.4.0"
-  sha256 "1ef7dd83d0f90ede2c2982a641fc27b6d58b1e8d448e985913b210747253ed63"
+  sha256 "56ff91cab2d5bd9dfbb68220a132fdd4a95fa325d7136ea90c04ebc77ab9c7b7"
 
-  conflicts_with "igblast-macosx", :because => "also ships igblastn, igblastp and makeblastdb binaries"
+  conflicts_with "igblast-linux", :because => "also ships igblastn, igblastp and makeblastdb binaries"
 
   def install
     prefix.install Dir["*"]
