d162de8ddd7a0cfe867697a97c40a75e8fe1e9dc
mikessh <mikhail.shugay@gmail.com>
2016-04-09 20:49:15 +0300

    checksums and dir structures

--- Formula/igblast-linux.rb
+++ Formula/igblast-linux.rb
@@ -3,15 +3,15 @@ class IgblastLinux < Formula
   homepage "http://www.ncbi.nlm.nih.gov/igblast/igblast.cgi"
   url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-linux-x64.zip"
   version "1.4.0"
-  sha256 "1ef7dd83d0f90ede2c2982a641fc27b6d58b1e8d448e985913b210747253ed63"
+  sha256 "e480a37556e31e0036cee3cca159ff1639a9f07727d88da53bc952e61e409637"
 
   conflicts_with "igblast-macos", :because => "also ships igblastn, igblastp and makeblastdb binaries"
 
   def install
     prefix.install Dir["*"]
-    bin.install_symlink "../bin/igblastn"
-    bin.install_symlink "../bin/igblastp"
-    bin.install_symlink "../bin/makeblastdb"
+    bin.install_symlink "../binaries/igblastn"
+    bin.install_symlink "../binaries/igblastp"
+    bin.install_symlink "../binaries/makeblastdb"
   end
 
   test do
--- Formula/igblast-macos.rb
+++ Formula/igblast-macos.rb
@@ -3,15 +3,15 @@ class IgblastMacos < Formula
   homepage "http://www.ncbi.nlm.nih.gov/igblast/igblast.cgi"
   url "https://github.com/mikessh/igblast-bin/releases/download/1.0/igblast-macosx.zip"
   version "1.4.0"
-  sha256 "56ff91cab2d5bd9dfbb68220a132fdd4a95fa325d7136ea90c04ebc77ab9c7b7"
+  sha256 "16b5bec47ff8c52cfaaac287e487e2fea62c41a037896845c8f3e74bc160a342"
 
   conflicts_with "igblast-linux", :because => "also ships igblastn, igblastp and makeblastdb binaries"
 
   def install
     prefix.install Dir["*"]
-    bin.install_symlink "../bin/igblastn"
-    bin.install_symlink "../bin/igblastp"
-    bin.install_symlink "../bin/makeblastdb"
+    bin.install_symlink "../binaries/igblastn"
+    bin.install_symlink "../binaries/igblastp"
+    bin.install_symlink "../binaries/makeblastdb"
   end
 
   test do
