6d0f58a729e55a8290741e5f63f44150e5cb2d3f
mikessh <mikhail.shugay@gmail.com>
2016-04-10 03:31:30 +0300

    Vdjtools with RInstall (note you may need link --overwrite a lot - it will install a brand new R)

--- Formula/vdjtools.rb
+++ Formula/vdjtools.rb
@@ -3,13 +3,16 @@ class Vdjtools < Formula
   homepage "http://vdjtools-doc.readthedocs.org"
   url "https://github.com/mikessh/vdjtools/releases/download/1.0.8/vdjtools-1.0.8.zip"
   version "1.0.8"
-  sha256 "3011e43b6128058e2f45fb5e8a43b16b35849ba417fae5ee9ad4ce08e34bea14"
+  sha256 "19dfd3e71c1bd9c199fea00c56c0a90c04de2cc6402e1ea3b9f592833867e90a"
 
   depends_on :java
+  depends_on "gcc"
+  depends_on "r"
 
   def install
     prefix.install Dir["*"]
     bin.install_symlink "../vdjtools"
+    system "#{bin}/vdjtools", "RInstall"
   end
 
   test do
