235dbb5ba60881f942606b59a845fef9017dc5b8
mikessh <mikhail.shugay@gmail.com>
2016-05-27 20:00:44 +0300

    2prev

--- Formula/vdjtools.rb
+++ Formula/vdjtools.rb
@@ -3,7 +3,7 @@ class Vdjtools < Formula
   homepage "http://vdjtools-doc.readthedocs.org"
   url "https://github.com/mikessh/vdjtools/releases/download/1.0.9/vdjtools-1.0.9.zip"
   version "1.0.9"
-  sha256 "97e8bba6bcce27d268a32c55b0d436e82464269236338886133bbdc2107152b8"
+  sha256 "5b68c701d7f91bf27bc1969e80388deebc07f738df7bfb4b853297e8f12a8d58"
 
   depends_on :java
   depends_on "gcc"
