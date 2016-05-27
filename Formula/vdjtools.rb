2627a529b2e41e99626d4b823d5c7ed624ce4990
mikessh <mikhail.shugay@gmail.com>
2016-05-27 19:43:04 +0300

    Update vdjtools formula

--- Formula/vdjtools.rb
+++ Formula/vdjtools.rb
@@ -1,9 +1,9 @@
 class Vdjtools < Formula
   desc "VDJtools: a framework for post-analysis of immune repertoire sequencing data."
   homepage "http://vdjtools-doc.readthedocs.org"
-  url "https://github.com/mikessh/vdjtools/releases/download/1.0.8/vdjtools-1.0.8.zip"
-  version "1.0.8"
-  sha256 "19dfd3e71c1bd9c199fea00c56c0a90c04de2cc6402e1ea3b9f592833867e90a"
+  url "https://github.com/mikessh/vdjtools/releases/download/1.0.9/vdjtools-1.0.9.zip"
+  version "1.0.9"
+  sha256 "97e8bba6bcce27d268a32c55b0d436e82464269236338886133bbdc2107152b8"
 
   depends_on :java
   depends_on "gcc"
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# homebrew-repseq
+### [Homebrew](http://brew.sh) repository for immune repertoire sequencing data analysis packages.
 
+```{bash}
 brew tap mikessh/repseq
 brew install migec
+brew install vdjtools
+```
