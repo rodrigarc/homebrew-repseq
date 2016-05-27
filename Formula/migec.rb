8744c53e08b0cdc486bc8e03be57eecd608a4c6b
mikessh <mikhail.shugay@gmail.com>
2016-05-27 20:21:01 +0300

    try other version format

--- Formula/migec.rb
+++ Formula/migec.rb
@@ -2,7 +2,7 @@ class Migec < Formula
   desc "MIGEC aka Molecular Identifier-Guided Error Correction pipeline. A set of tools for working with unique molecular identifier (UMI)-tagged high-throughput sequencing data. The primary focus is T- and B-cell receptor sequencing."
   homepage "http://migec.readthedocs.org/en/latest/index.html"
   url "https://github.com/mikessh/migec/releases/download/1.2.4a/migec-1.2.4a.zip"
-  version "1.2.4a"
+  version "1.2.4-1"
   sha256 "2af2a0bcc1965d40bd308b1eb67d8144062f8063621cd8faab90b71f2c4e52e4"
 
   depends_on :java
