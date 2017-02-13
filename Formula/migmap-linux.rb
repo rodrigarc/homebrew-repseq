0ddd41bda5cf7ee25a4c1e636f221ad935e5324a
mikessh <mikhail.shugay@gmail.com>
2017-02-14 00:56:45 +0300

    Update migmap formula

--- Formula/migmap-linux.rb
+++ Formula/migmap-linux.rb
@@ -1,9 +1,9 @@
 class MigmapLinux < Formula
   desc "MIGMAP: HTS-compatible wrapper for IgBlast V-(D)-J mapping tool."
   homepage "https://github.com/mikessh/migmap"
-  url "https://github.com/mikessh/migmap/releases/download/1.0.1/migmap-1.0.1.zip"
-  version "1.0.1"
-  sha256 "11ccbd5f2ea801676df41c5c1c75f0a1d52479a98c87ec2223f446467054f8f2"
+  url "https://github.com/mikessh/migmap/releases/download/1.0.2/migmap-1.0.2.zip"
+  version "1.0.2"
+  sha256 "35936c19bb0ac8b59322ee4e602e92e90fc1afdcf0dd5018521a4b28f09f7842"
 
   depends_on "igblast-linux"
   
