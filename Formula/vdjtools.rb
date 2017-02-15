9112cfa43b48bcdbae5ea1b030ca51bba592fef0
mikessh <mikhail.shugay@gmail.com>
2017-02-15 17:15:56 +0300

    Update vdjtools formula

--- Formula/vdjtools.rb
+++ Formula/vdjtools.rb
@@ -1,9 +1,9 @@
 class Vdjtools < Formula
   desc "VDJtools: a framework for post-analysis of immune repertoire sequencing data."
   homepage "http://vdjtools-doc.readthedocs.org"
-  url "https://github.com/mikessh/vdjtools/releases/download/1.1.3/vdjtools-1.1.3.zip"
-  version "1.1.3"
-  sha256 "0118f0aee26a176ef998bc48f993e4d152ba5c0572bb43d709fb675bee95df14"
+  url "https://github.com/mikessh/vdjtools/releases/download/1.1.4/vdjtools-1.1.4.zip"
+  version "1.1.4"
+  sha256 "e5b85813aaccf1e37c3e1f9f61936ab282aec5a37455a7094ae6f5b8d229b4cb"
 
   depends_on :java
   depends_on "gcc"
