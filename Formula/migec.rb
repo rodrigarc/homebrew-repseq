2c228a2d3cd9e7d42dce76fcf173505468d615b6
mikessh <mikhail.shugay@gmail.com>
2017-02-16 03:09:31 +0300

    Update MIGEC formula, include BLAST there

--- Formula/migec.rb
+++ Formula/migec.rb
@@ -1,11 +1,12 @@
 class Migec < Formula
   desc "MIGEC aka Molecular Identifier-Guided Error Correction pipeline. A set of tools for working with unique molecular identifier (UMI)-tagged high-throughput sequencing data. The primary focus is T- and B-cell receptor sequencing."
   homepage "http://migec.readthedocs.org/en/latest/index.html"
-  url "https://github.com/mikessh/migec/releases/download/1.2.5/migec-1.2.5.zip"
-  version "1.2.5"
-  sha256 "c2107359a8766b0afd794a38089564367200b5790f9cc06180d60f0f6c6156d1"
+  url "https://github.com/mikessh/migec/releases/download/1.2.6/migec-1.2.6.zip"
+  version "1.2.6"
+  sha256 "a9872d6773cee978753359d6d44886cfc42f39d9ff8d85c63cd5c47ffdbd2ddc"
 
   depends_on :java
+  depends_on "blast"
 
   def install
     prefix.install Dir["*"]
